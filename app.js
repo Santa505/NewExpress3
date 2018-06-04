0e6400373cef9f4f06f912e407d0773dba2c49c8
jeff indall <jeffindall@gmail.com>
2018-06-04 10:43:07 -0600

    last commit

--- app.js
+++ app.js
@@ -36,9 +36,11 @@ app.listen(port, () => {
           message: "No record found!"
         }
       });
-    }
+    } else {
 
-    response.json({ data: record });
+  response.json({ data: record });
+
+  }
 
 
   })
